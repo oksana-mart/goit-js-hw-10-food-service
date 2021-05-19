abcee49dc0b662b2fdb8f5cbe81042cb8e80745f
Oksana Martynenko <oks.martynenko@gmail.com>
2021-05-19 22:38:34 +0300

    Добавляет js функционал

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-import './sass/main.scss';
+import menu from './menu.json';
+import menuCardTemolate from './menu-card-template.hbs';
+import './styles.css';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuContainer = document.querySelector('.js-menu');
+const menuMarkup = createMenuCardsMarkup(menu);
+
+menuContainer.insertAdjacentHTML('beforeend', menuMarkup);
+
+function createMenuCardsMarkup(menu) {
+  return menuCardTemolate(menu);
+};
+
+const body = document.querySelector('body');
+const themeSwitcher = document.querySelector('#theme-switch-toggle');
+body.classList.add("light-theme");
+
+themeSwitcher.addEventListener('input', onThemeSwitcherChange);
+
+function onThemeSwitcherChange(event) {
+  const position = event.currentTarget.value;
+
+  if (position) {
+    body.classList.toggle(Theme.LIGHT);
+    body.classList.toggle(Theme.DARK);
+  };
+
+  localStorage.setItem('switcher-position', (body.classList.value));
+};
+
+getTheme();
+
+function getTheme() {
+  const savedTheme = localStorage.getItem('switcher-position');
+
+  if (savedTheme === Theme.DARK) {
+    body.classList = savedTheme;
+    themeSwitcher.setAttribute('checked', 'true');
+  }
+};
